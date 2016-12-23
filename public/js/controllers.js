cb663105d18572a2823ca44489ed0177cc453f58
Brett L Kleinschmidt <blk@blk.me>
2016-12-23 11:09:45 -0800

    Disable initial search

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,6 @@
 angular.module('GMPDApp', [])
     .controller('SearchController', function($scope, $http) {
         $scope.search = function() {
-            console.log($scope.query);
             if ($scope.query) {
                 $http({
                     method: 'GET',
@@ -9,11 +8,9 @@ angular.module('GMPDApp', [])
                 }).then(function successCallback(res) {
                     $scope.tracks = res.data.entries.filter((x) => x.type == 1).map((x) => x.track);
                     $scope.artists = res.data.entries.filter((x) => x.type == 2).map((x) => x.artist);
-                    console.log($scope.tracks);
                 }, function errorCallback(res) {
                     console.log(res);
                 });
             }
         }
-        $scope.search();
     })
