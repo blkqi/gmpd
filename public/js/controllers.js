2cb9a22260b744f09a1cf6a0317ac3e25d5fdb6b
Cory Kleinschmidt <clkmsc@gmail.com>
2016-12-23 10:25:17 -0800

    Removed console log

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,7 +6,6 @@ angular.module('GMPDApp', [])
         }).then(function successCallback(res) {
             $scope.tracks = res.data.entries.filter((x) => x.type == 1).map((x) => x.track);
             $scope.artists = res.data.entries.filter((x) => x.type == 2).map((x) => x.artist);
-            console.log($scope.tracks);
         }, function errorCallback(res) {
             console.log(res);
         });
