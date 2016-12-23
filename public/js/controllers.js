e06ccc159803d9755b01ce33befe97f6cd72cb5d
Brett L Kleinschmidt <blk@blk.me>
2016-12-23 11:09:02 -0800

    Wire up search with angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,13 +1,19 @@
 angular.module('GMPDApp', [])
     .controller('SearchController', function($scope, $http) {
-        $http({
-            method: 'GET',
-            url: '/search?q=carl+craig'
-        }).then(function successCallback(res) {
-            $scope.tracks = res.data.entries.filter((x) => x.type == 1).map((x) => x.track);
-            $scope.artists = res.data.entries.filter((x) => x.type == 2).map((x) => x.artist);
-        }, function errorCallback(res) {
-            console.log(res);
-        });
+        $scope.search = function() {
+            console.log($scope.query);
+            if ($scope.query) {
+                $http({
+                    method: 'GET',
+                    url: '/search?q=' + encodeURIComponent($scope.query)
+                }).then(function successCallback(res) {
+                    $scope.tracks = res.data.entries.filter((x) => x.type == 1).map((x) => x.track);
+                    $scope.artists = res.data.entries.filter((x) => x.type == 2).map((x) => x.artist);
+                    console.log($scope.tracks);
+                }, function errorCallback(res) {
+                    console.log(res);
+                });
+            }
+        }
+        $scope.search();
     })
-
