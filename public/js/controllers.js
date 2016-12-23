dbc5e074ff2a31b21e0cf38177742552748ef1e1
Cory Kleinschmidt <clkmsc@gmail.com>
2016-12-23 12:16:58 -0800

    Slider fix for angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,3 +14,48 @@ angular.module('GMPDApp', [])
             }
         }
     })
+
+.directive('lightslider', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      if (scope.$last) {
+        element.parent().lightSlider({
+			autoWidth: false,
+			item:4,
+			slideMove:2,
+			slideMargin: 10,
+			mode: "slide",
+			useCSS: true,
+			speed: 400, //ms'
+			auto: false,
+			loop: false,
+			slideEndAnimation: true,
+			pause: 2000,
+			pager: true,
+			enableTouch:true,
+			enableDrag:true,
+			freeMove:true,
+			swipeThreshold: 40,
+			pager: false,
+			responsive : [
+				{
+					breakpoint:900,
+					settings: {
+						item:3,
+						slideMove:1
+					  }
+				},
+				{
+					breakpoint:700,
+					settings: {
+						item:2,
+						slideMove:1
+					  }
+				}
+			]
+		});
+      }
+    }
+  };
+});
