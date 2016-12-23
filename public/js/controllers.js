67609b9a40004a5606c8478098f5402549895ae9
Brett L Kleinschmidt <blk@blk.me>
2016-12-23 10:19:16 -0800

    Enable artists

---
+++ public/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('GMPDApp', [])
+    .controller('SearchController', function($scope, $http) {
+        $http({
+            method: 'GET',
+            url: '/search?q=carl+craig'
+        }).then(function successCallback(res) {
+            $scope.tracks = res.data.entries.filter((x) => x.type == 1).map((x) => x.track);
+            $scope.artists = res.data.entries.filter((x) => x.type == 2).map((x) => x.artist);
+            console.log($scope.tracks);
+        }, function errorCallback(res) {
+            console.log(res);
+        });
+    })
+
