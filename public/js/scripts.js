1a0f57df05f8a19b99ef982b0c8a7f22c1c57a4b
Brett L Kleinschmidt <blk@blk.me>
2016-12-24 09:31:55 -0800

    Remove uneeded jQuery scripts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,39 +1,12 @@
 $(document).ready(function(){
-    
     $(window).on('load', function(){ updateView() });
     $(window).on('resize', function(){ updateView() });
-    
-    $('.btn.track').click( function () {
-      var data = {
-        "mode": $(this).attr('data-mode'),
-        "type": "track",
-        "track": [ $(this).attr('data-track') ],
-        "id": null
-      };
-      $.post("load", data, function(data, status){
-          //$.notify(data,"success");
-          alert('success');
-      });
-    });
-    
-    $('div.frm input').focus(function(){
-    	$(this).select();
-	});
-	
-    //$("form#search input").val(decodeURIComponent(urlParam('q').replace(/\+/g, '%20')));
-
+    $('div.frm input').focus(function(){ $(this).select(); });
 });
 
 function updateView() {
-	if ( $( window ).width() < 600 ) {
-		$('body').addClass('mini');
-	}
-	else {
-		$('body').removeClass('mini');
-	}
-}
-
-function urlParam(name) {
-  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-  return results ? results[1] : '';
+	if ($( window ).width() < 600)
+        $('body').addClass('mini');
+	else
+        $('body').removeClass('mini');
 }
