80f8d4867de3832b1f5668deb12d9641a2db6cba
Brett L Kleinschmidt <blk@blk.me>
2016-12-25 08:35:47 -0800

    Integrate browserify

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+ 
+var angular = require('angular');
+var app = angular.module('app', []);
+ 
+//require('./config');
+require('./controllers');
+require('./lightslider');
+require('./directives');
